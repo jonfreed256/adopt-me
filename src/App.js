881c328fa225a4cbfba55030446afad69822ae75
github username <github email address>
2019-07-22 15:11:18 -0400

    custom hook useDropdown

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  //   return React.createElement("div", {}, [
-  //     React.createElement("h1", {}, "Adopt Me!"),
-  //     React.createElement(Pet, {
-  //       name: "Madison & Mazie",
-  //       animal: "dog",
-  //       breed: "mixed"
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Harper",
-  //       animal: "dog",
-  //       breed: "mixed"
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Pepper",
-  //       animal: "dog",
-  //       breed: "black lab"
-  //     })
-  //   ]);
-
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Mazie" animal="dog" breed="mixed" />
-      <Pet name="Madison" animal="dog" breed="mixed" />
-      <Pet name="Harper" animal="dog" breed="mixed" />
+      <SearchParams />
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seatle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "Dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  return (
+    <div className="search-params">
+      <form action="">
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            type="text"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
