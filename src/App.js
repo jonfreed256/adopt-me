b92f905d9a344822720d922feb1e5a143cf3bdac
github username <github email address>
2019-07-22 13:52:07 -0400

    refactored Pet.js and App.js with jsx

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Madison & Mazie",
-      animal: "dog",
-      breed: "mixed"
-    }),
-    React.createElement(Pet, {
-      name: "Harper",
-      animal: "dog",
-      breed: "mixed"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "dog",
-      breed: "black lab"
-    })
-  ]);
-};
+  //   return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, "Adopt Me!"),
+  //     React.createElement(Pet, {
+  //       name: "Madison & Mazie",
+  //       animal: "dog",
+  //       breed: "mixed"
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Harper",
+  //       animal: "dog",
+  //       breed: "mixed"
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Pepper",
+  //       animal: "dog",
+  //       breed: "black lab"
+  //     })
+  //   ]);
 
-render(React.createElement(App), document.getElementById("root"));
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Mazie" animal="dog" breed="mixed" />
+      <Pet name="Madison" animal="dog" breed="mixed" />
+      <Pet name="Harper" animal="dog" breed="mixed" />
+    </div>
+  );
+};
+render(<App />, document.getElementById("root"));
